c602c73cb16b1946f66de3b7e1caaa2d43005120
Hiroki2728 <moxx.jh@gmail.com>
2021-03-09 22:29:31 +0900

    Update Lesson11.go

--- Lesson11.go
+++ Lesson11.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <= 4; i++ {
-		if i == 3 {
-			continue
+	for i := 0; i <= 2; i++ {
+		for j := 0; j <= 2; j++ {
+			fmt.Println(i, "-", j)
 		}
-		fmt.Println(i)
 	}
 }
